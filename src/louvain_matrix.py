256074ab3df99d8a23a053d304364e2aa0fe9942
elodiegermani1 <elodie.germani@irisa.fr>
2023-06-29 11:48:54 +0200

    Louvain for subjects

--- src/louvain_matrix.py
+++ src/louvain_matrix.py
@@ -21,20 +21,38 @@ def main():
 	data_path = '/srv/tempdd/egermani/hcp_many_pipelines'
 	contrast = 'left-foot'
 
-	mask = louvain_utils.compute_intersection_mask(data_path, contrast)
-	Qs = louvain_utils.compute_correlation_matrix(data_path, contrast, mask)
-	partitioning = louvain_utils.per_group_partitioning(Qs)
-	matrix_graph, subject = louvain_utils.compute_partition_matrix(data_path, contrast, partitioning)
+	data_type='subject'
 
-	G = nx.Graph(matrix_graph, seed=0)
-	# compute the best partition
-	partition = community_louvain.best_partition(G, random_state=0)
-	#saving_name = '{}/graph_community_alltogether.png'.format(results_dir)
-	title_graph = "Community of HCP many pipelines"
-	title_heatmap = "Heatmap (Louvain organized) based on occurence \nof belonging to the same community across each group-level analysis"
-	saving_names = [f'/srv/tempdd/egermani/pipeline_distance/figures/graph_1000_groups_{contrast}.png',f'/srv/tempdd/egermani/pipeline_distance/figures/heatmap_1000_groups_{contrast}.png']
+	if data_type =='group':
+		mask = louvain_utils.compute_intersection_mask(data_path, contrast)
+		Qs = louvain_utils.compute_correlation_matrix(data_path, contrast, mask)
+		partitioning = louvain_utils.per_group_partitioning(Qs)
+		matrix_graph, subject = louvain_utils.compute_partition_matrix(data_path, contrast, partitioning)
 
-	louvain_utils.build_both_graph_heatmap(matrix_graph, G, partition, subject, "All", saving_names, contrast)
-        
+		G = nx.Graph(matrix_graph, seed=0)
+		# compute the best partition
+		partition = community_louvain.best_partition(G, random_state=0)
+		#saving_name = '{}/graph_community_alltogether.png'.format(results_dir)
+		title_graph = "Community of HCP many pipelines"
+		title_heatmap = "Heatmap (Louvain organized) based on occurence \nof belonging to the same community across each group-level analysis"
+		saving_names = [f'/srv/tempdd/egermani/pipeline_distance/figures/graph_1000_groups_{contrast}.png',f'/srv/tempdd/egermani/pipeline_distance/figures/heatmap_1000_groups_{contrast}.png']
+
+		louvain_utils.build_both_graph_heatmap(matrix_graph, G, partition, subject, "All", saving_names, contrast)
+
+	else: 
+        mask = louvain_subject.compute_intersection_mask(data_path, contrast)
+		Qs = louvain_subject.compute_correlation_matrix(data_path, contrast, mask)
+		partitioning = louvain_subject.per_group_partitioning(Qs)
+		matrix_graph, subject = louvain_subject.compute_partition_matrix(data_path, contrast, partitioning)
+
+		G = nx.Graph(matrix_graph, seed=0)
+		# compute the best partition
+		partition = community_louvain.best_partition(G, random_state=0)
+
+		saving_names = [f'/srv/tempdd/egermani/pipeline_distance/figures/graph_1080_subs_{contrast}.png',
+		f'/srv/tempdd/egermani/pipeline_distance/figures/heatmap_1080_subs_{contrast}.png']
+
+		louvain_subject.build_both_graph_heatmap(matrix_graph, G, partition, subject, "All", saving_names, contrast)
+		
 if __name__ == '__main__':
 	main()
